aec5c2a6d0f65d0ff9dbe20867486f0a92b40ed2
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-12-04 15:23:20 +0100

    add input

--- vite-project/src/assets/components/Main.jsx
+++ vite-project/src/assets/components/Main.jsx
@@ -2,4 +2,10 @@ import { useState } from "react";
 import PostList from "./PostList";
 import { posts as data } from "../data/posts";
 
-export default function Main() { }
+export default function Main() {
+    const [titleInput, setTitleInput] = useState("");
+    const [authorInput, setAuthorInput] = useState("");
+    const [statusInput, setStatusInput] = useState("");
+    const [contentInput, setContentInput] = useState("");
+
+}
