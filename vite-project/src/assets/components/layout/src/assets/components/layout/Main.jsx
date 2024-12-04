46d3e98f5768f5dd5ceb665c033e9836edbce4b5
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-12-04 17:15:03 +0100

    add form

--- vite-project/src/assets/components/layout/src/assets/components/layout/Main.jsx
+++ vite-project/src/assets/components/layout/src/assets/components/layout/Main.jsx
@@ -1,3 +1,12 @@
 import { useState, useEffect } from "react";
 import CardList from "../CardList";
 import { posts } from "../../data/posts";
+
+const formInitialData = {
+    author: "",
+    content: "",
+    isPublic: false,
+    tags: [],
+    title: "",
+    category: "",
+};
